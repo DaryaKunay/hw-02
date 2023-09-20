61f09bf6bce9f4d4ca79562c4cfb8eb965e4a903
DaryaKunay <145052008+DaryaKunay@users.noreply.github.com>
2023-09-20 22:12:52 +0300

    c

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿byte myB=15;
+  string input = Console.ReadLine();
+               byte myinput = byte.Parse(input);
+               Console.WriteLine($"You number = {input}");
+
+Console.WriteLine ("|Название типа данных|Размер в памяти|Пример|Максимум|Минимум|");
+Console.WriteLine ( "| byte | 8 |"+ myB "| Максимум | Минимум |");
+Console.WriteLine (| Название типа данных |Размер в памяти|Пример|Максимум|Минимум|);
+Console.WriteLine (| Название типа данных |Размер в памяти|Пример|Максимум|Минимум|);
+Console.WriteLine (|Название типа данных|Размер в памяти|Пример|Максимум|Минимум|);
+Console.WriteLine (|Название типа данных|Размер в памяти|Пример|Максимум|Минимум|);
+Console.WriteLine (|Название типа данных|Размер в памяти|Пример|Максимум|Минимум|);
+Console.WriteLine (| Название типа данных |Размер в памяти|Пример|Максимум|Минимум|);
